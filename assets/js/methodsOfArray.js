3fc183f8f2d3dfac4d75c0aa0f1ddcb6182ba00a
Sriramkumar Jothimani <sriramkumar@kiranum.com>
2020-07-21 11:12:58 +0530

    Add - Array reduce method

--- assets/js/methodsOfArray.js
+++ assets/js/methodsOfArray.js
@@ -3,48 +3,48 @@
 let numbers = [1, 2, 3];
 numbers = numbers.concat(['a','b']);
 
-console.log(numbers);
+console.log('Concat :',numbers);
 
 // Join
 
 test = numbers.join('-');
-console.log(test);
+console.log('Join :',test);
 
 // Push
 
 numbers.push(4);
-console.log(numbers);
+console.log('Push :',numbers);
 
 
 // Pop
 
 console.log(numbers.pop());
-console.log(numbers);
+console.log('Pop :',numbers);
 
 // Shift
 numbers.shift();
-console.log(numbers);
+console.log('Shift :', numbers);
 
 // Un-shift
 numbers.unshift(0);
-console.log(numbers);
+console.log('Unshift :',numbers);
 
 // Slice
 test1 = numbers.slice(1,2);
-console.log(test1);
+console.log('Slice :', test1);
 
 // Splice
 test2 = numbers.splice(1,2,-1,-2);
-console.log(test2);
-console.log(numbers);
+console.log('Splice :', test2);
+console.log('Spliced array :', numbers);
 
 // Reverse
 numbers.reverse();
-console.log(numbers);
+console.log('Reverse :', numbers);
 
 // Sort an array
 let nums = [1,3,2,5,6,4,3,2];
-console.log(nums.sort());
+console.log('Sort :' ,nums.sort());
 
 let students = [
     {
@@ -92,4 +92,49 @@ students.sort((a, b) => {
     return 0;
 });
 
-console.log(students);
+console.log('Sort with compare :',students);
+
+// Reduce
+
+const studentsArray = [{
+        name: 'a',
+        marks: 10,
+        class: 'I'
+    },{
+        name: 'b',
+        marks: 15,
+        class: 'J'
+    },{
+        name: 'c',
+        marks: 16,
+        class: 'I'
+    },{
+        name: 'd',
+        marks: 12,
+        class: 'J'
+    },{
+        name: 'e',
+        marks: 13,
+        class: 'I'
+    },{
+        name: 'f',
+        marks: 10,
+        class: 'J'
+    }];
+
+let iSectionStuds = [];
+
+function accumulator(iSectionStuds, student) {
+    console.log('1:',iSectionStuds);
+    console.log('2:',student);
+    if (student.class === 'I' && student.marks > 10) {
+        iSectionStuds.push(student);
+    }
+    return iSectionStuds;
+}
+
+iSectionStuds = studentsArray.reduce(accumulator, []);
+
+console.log('Students of I with marks > 10,',iSectionStuds);
+
+
